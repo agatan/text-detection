6b8fca1179f62cd34decc478d4e68562249a8cde
agatan <agatan039@gmail.com>
2018-11-30 17:55:29 +0900

    MobileNetV2 based pixel link

--- net.py
+++ net.py
@@ -48,6 +48,120 @@ class PixelLink(nn.Module):
         return o[:, :2, :, :], o[:, 2:, :, :]
 
 
+def _conv_bn(in_channels, out_channels, stride):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True),
+    )
+
+
+def _conv1x1_bn(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True),
+    )
+
+
+class _InvertedResidual(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio):
+        super(_InvertedResidual, self).__init__()
+        self.use_residual = stride == 1 and in_channels == out_channels
+        if expand_ratio == 1:
+            self.conv = nn.Sequential(
+                # depthwise
+                nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, bias=False, groups=in_channels),
+                nn.BatchNorm2d(in_channels),
+                nn.ReLU6(inplace=True),
+                # pointwise
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        else:
+            hidden_dim = round(in_channels * expand_ratio)
+            self.conv = nn.Sequential(
+                # pointwise
+                nn.Conv2d(in_channels, hidden_dim, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # depthwise
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, bias=False, groups=hidden_dim),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pointwise
+                nn.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.conv(x)
+        return self.conv(x)
+
+
+class MobileNetV2PixelLink(nn.Module):
+    def __init__(self, scale):
+        super(MobileNetV2PixelLink, self).__init__()
+        assert scale in [2, 4]
+        self.scale = scale
+        first_channels = 32
+        inverted_residual_config = [
+            # t (expand ratio), channels, n (layers), stride
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+        features = [_conv_bn(3, first_channels, stride=2)]
+        input_channels = first_channels
+        for i, (t, c, n, s) in enumerate(inverted_residual_config):
+            output_channels = c
+            layers = []
+            for j in range(n):
+                if j == 0:
+                    layers.append(_InvertedResidual(input_channels, output_channels, stride=s, expand_ratio=t))
+                else:
+                    layers.append(_InvertedResidual(input_channels, output_channels, stride=1, expand_ratio=t))
+                input_channels = output_channels
+            features.append(nn.Sequential(*layers))
+        last_channels = 1280
+        features.append(_conv1x1_bn(input_channels, last_channels))
+        self.block1 = nn.Sequential(*features[:2])
+        self.block2 = nn.Sequential(*features[2:3])
+        self.block3 = nn.Sequential(*features[3:4])
+        self.block4 = nn.Sequential(*features[4:6])
+        self.block5 = nn.Sequential(*features[6:])
+        out_channels = 2 + 8 * 2  # text/non-text, 8 neighbors
+        self.out_conv1 = nn.Conv2d(1280, out_channels, kernel_size=1)
+        self.out_conv2 = nn.Conv2d(96, out_channels, kernel_size=1)
+        self.out_conv3 = nn.Conv2d(32, out_channels, kernel_size=1)
+        self.out_conv4 = nn.Conv2d(24, out_channels, kernel_size=1)
+        if self.scale == 2:
+            self.out_conv5 = nn.Conv2d(16, out_channels, kernel_size=1)
+        self.final_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        x3 = self.block3(x2)
+        x4 = self.block4(x3)
+        x5 = self.block5(x4)
+        o5 = self.out_conv1(x5)
+        o4 = self.out_conv2(x4) + F.interpolate(o5, scale_factor=2)
+        o3 = self.out_conv3(x3) + F.interpolate(o4, scale_factor=2)
+        o2 = self.out_conv4(x2) + F.interpolate(o3, scale_factor=2)
+        if self.scale == 2:
+            o1 = self.out_conv5(x1) + F.interpolate(o2, scale_factor=2)
+        else:
+            o1 = o2
+        o = self.final_conv(o1)
+        return o
+
+
 class PixelLinkLoss:
     def __init__(self, pixel_input, pixel_target, neg_pixel_masks, pixel_weight, link_input, link_target, r=3, l=2):
         self._set_pixel_weight_and_loss(pixel_input, pixel_target, neg_pixel_masks, pixel_weight, r=r)
