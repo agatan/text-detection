ef7d53442fea97532931a7a00d64a2d75f110ed0
agatan <agatan039@gmail.com>
2018-12-01 16:03:19 +0900

    Cleanup prediction script

--- predict.py
+++ predict.py
@@ -1,50 +1,75 @@
 import os
+from typing import Tuple
 
 import torch
 import torch.utils.data as data
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
 from data import ICDAR15Dataset
 import net
+import postprocess
+
+
+def resize_image(image: np.ndarray, size=(512, 512)) -> Tuple[np.ndarray, float, float]:
+    height, width, _ = image.shape
+    image = cv2.resize(image, size)
+    height_ratio = height / float(size[1])
+    width_ratio = width / float(size[0])
+    return (image, height_ratio, width_ratio)
+
+
+def preprocess_image(image: np.ndarray) -> torch.Tensor:
+    image = np.transpose(image, (2, 0, 1))
+    return torch.Tensor(image.astype(np.float32) / 255.)
+
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train", default="./dataset/icdar2015")
-    parser.add_argument("--restore")
+    parser.add_argument("image")
+    parser.add_argument("--checkpoint", required=True)
+    parser.add_argument("--scale", default=4, type=int)
     args = parser.parse_args()
-    dataset = ICDAR15Dataset(os.path.join(args.train, "images"), os.path.join(args.train, "labels"), image_size=(512, 512), scale=4)
-    dataloader = data.DataLoader(dataset, batch_size=1, shuffle=True)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # pixellink = net.PixelLink(args.scale, pretrained=False).to(device)
-    pixellink = net.MobileNetV2PixelLink(4).to(device)
-    pixellink.load_state_dict(torch.load(args.restore))
+    pixellink = net.MobileNetV2PixelLink(scale=args.scale)
+    pixellink.load_state_dict(torch.load(args.checkpoint, map_location=lambda storage, loc: storage))
     pixellink.eval()
 
-    with torch.no_grad():
-        for images, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_masks in dataloader:
-            images = images.to(device)
-            pos_pixel_masks = pos_pixel_masks.to(device)
-            neg_pixel_masks = neg_pixel_masks.to(device)
-            pixel_weights = pixel_weights.to(device)
-            link_masks = link_masks.to(device)
-            pixel_prediction, link_prediction = pixellink(images)
-            pixel_prediction = torch.softmax(pixel_prediction, dim=1)
-            pixel_map = pixel_prediction[0, 1, :, :].numpy()
-            # pixel_map += 0.3
-            neighbor = 2
-            link_prediction = torch.softmax(link_prediction[:, 2*neighbor: 2*(neighbor+1)], dim=1)
-            link_map = link_prediction[0, 1, :, :].numpy() * pos_pixel_masks[0].numpy()
-            link_map /= link_map.max()
-            # link_map += 0.3
-            m = link_map
-            # m = pixel_map
-            image = images.transpose(1, 2).transpose(2, 3).numpy()[0]
-            out = image * np.expand_dims(cv2.resize(m, image.shape[:2]), -1)
-            plt.imshow(out)
-            plt.show()
+    image = cv2.imread(args.image, cv2.IMREAD_COLOR)
+    resized_image, height_ratio, width_ratio = resize_image(image)
+    image_tensor = preprocess_image(resized_image).unsqueeze(0)
 
-main()
+    with torch.no_grad():
+        pixel_pred, link_pred = pixellink(image_tensor)
+        instance_map = postprocess.mask_to_instance_map(pixel_pred, link_pred)
+        bounding_boxes = postprocess.instance_map_to_bboxes(instance_map, scale=args.scale)
+
+    instance_map = instance_map[0]
+    bounding_boxes = bounding_boxes[0]
+
+    # colorize instance map.
+    colored_instance_map = np.zeros(instance_map.shape + (3,), np.uint8)
+    n_instances = np.max(instance_map)
+    for n in range(1, n_instances + 1):
+        colors = (np.random.random(3) * 255).astype(np.uint8)
+        for y, x in zip(*np.where(instance_map == n)):
+            colored_instance_map[y, x, :] = colors
+    colored_instance_map = cv2.resize(colored_instance_map, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    cv2.imwrite("instances.png", colored_instance_map)
+
+    # draw boxes
+    for box in bounding_boxes:
+        box[:, 0] *= height_ratio
+        box[:, 1] *= width_ratio
+        xy_box = np.zeros_like(box)
+        xy_box[:, 0] = box[:, 1]
+        xy_box[:, 1] = box[:, 0]
+        xy_box = xy_box.astype(np.int32)
+        image = cv2.drawContours(image, [xy_box], 0, (0, 0, 255), thickness=2)
+    cv2.imwrite("predict.png", image)
+
+
+if __name__ == '__main__':
+    main()
