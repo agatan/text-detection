3e56bed82e0a05978d562ff649fc5358256c248c
agatan <agatan039@gmail.com>
2018-12-02 04:08:19 +0900

    Fix neighbors definition

--- data.py
+++ data.py
@@ -151,6 +151,7 @@ class ICDAR15Dataset(data.Dataset):
                 [y, x - 1],
                 [y, x + 1],
                 [y + 1, x - 1],
+                [y + 1, x],
                 [y + 1, x + 1],
             ]
 
