b80fa01da5035accb91e70bc9d73e7cc0a03ad4d
agatan <agatan039@gmail.com>
2018-12-03 21:29:33 +0900

    Rewrite the training script with ignite

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 import os
+import warnings
 
 import torch
 import torch.utils.data as data
+from ignite.engine import Engine, Events
+from ignite.handlers import ModelCheckpoint, Timer
+from ignite.contrib.handlers import ProgressBar
+from ignite.metrics import RunningAverage
 from tensorboardX import SummaryWriter
 import numpy as np
 from tqdm import tqdm
@@ -11,47 +16,7 @@ from data import ICDAR15Dataset
 import net
 
 
-class Reporter:
-    def __init__(self, name, writer, frequency=None):
-        self.name = name
-        self.losses = []
-        self.pixel_losses = []
-        self.pixel_accuracies = []
-        self.link_losses = []
-        self.link_accuracies = []
-        self.writer = writer
-        self.frequency = frequency
-
-    def step(self, loss_object, steps):
-        self.losses.append(loss_object.loss.item())
-        self.pixel_losses.append(loss_object.pixel_loss.item())
-        self.pixel_accuracies.append(loss_object.pixel_accuracy)
-        self.link_losses.append(loss_object.link_loss.item())
-        self.link_accuracies.append(loss_object.link_accuracy)
-        if self.frequency is not None and len(self.losses) == self.frequency:
-            self.flush(steps)
-
-    def flush(self, steps):
-        print("Report {}: Step {}".format(self.name, steps))
-        print("Loss: {} (Pixel: {}, Link: {})".format(np.mean(self.losses), np.mean(self.pixel_losses), np.mean(self.link_losses)))
-        print("Pixel Accuracy: {:.4f}, Link Accuracy: {:.4f}".format(np.mean(self.pixel_accuracies), np.mean(self.link_accuracies)))
-        self._write("loss", self.losses, steps)
-        self._write("loss/pixel", self.pixel_losses, steps)
-        self._write("loss/link", self.link_losses, steps)
-        self._write("accuracy/pixel", self.pixel_accuracies, steps)
-        self._write("accuracy/link", self.link_accuracies, steps)
-        self._reset()
-
-    def _reset(self):
-        self.losses = []
-        self.pixel_losses = []
-        self.pixel_accuracies = []
-        self.link_losses = []
-        self.link_accuracies = []
-
-    def _write(self, name, value, steps):
-        self.writer.add_scalar(name, np.mean(value), steps)
-
+LOG_FREQ = 200
 
 
 def main():
@@ -76,61 +41,37 @@ def main():
     if args.test is not None:
         test_dataset = ICDAR15Dataset(os.path.join(args.test, "images"), os.path.join(args.test, "labels"), image_size=image_size, scale=args.scale, training=False)
     else:
-        n_train = int(len(dataset) * 0.95)
-        dataset, test_dataset = torch.utils.data.random_split(dataset, [n_train, len(dataset) - n_train])
+        n_test = min(1000, (len(dataset) * 0.05))
+        indices = np.arange(len(dataset))
+        dataset = torch.utils.data.Subset(dataset, indices[n_test:])
+        test_dataset = torch.utils.data.Subset(dataset, indices[:n_test])
+        print(len(dataset), len(test_dataset))
     test_dataloader = data.DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=8)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # pixellink = net.PixelLink(args.scale, pretrained=False).to(device)
-    pixellink = net.MobileNetV2PixelLink(args.scale).to(device)
-    optimizer = torch.optim.Adam(pixellink.parameters(), lr=1e-3)
     # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-    steps = 0
-    start_epoch = 0
-    best_score = None
     if args.restore:
         if torch.cuda.is_available():
             map_location = None
         else:
             def map_location(storage, loc):
                 return storage
-        state_dict = torch.load(args.restore, map_location=map_location)
-        pixellink.load_state_dict(state_dict['pixellink'])
-        optimizer.load_state_dict(state_dict['optimizer'])
-        steps = state_dict["steps"]
-        start_epoch = state_dict["epoch"]
-        args.scale = state_dict["scale"]
-        best_score = state_dict["best_score"]
+        pixellink = torch.load(args.restore, map_location=map_location).to(device)
+    else:
+        pixellink = net.MobileNetV2PixelLink(args.scale).to(device)
+    optimizer = torch.optim.Adam(pixellink.parameters(), lr=1e-3)
 
-    os.makedirs(args.checkpoint, exist_ok=True)
-    writer = SummaryWriter(os.path.join(args.logdir, "train"))
-    reporter = Reporter("train", writer, 200)
-    test_writer = SummaryWriter(os.path.join(args.logdir, "test"))
-    test_reporter = Reporter("test", test_writer)
-    for epoch in range(start_epoch, args.epochs):
-        print("[Epoch {}]".format(epoch))
-        steps_per_epoch = (len(dataset) - 1) // args.batch_size + 1
-        for images, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_masks in tqdm(itertools.islice(dataloader, steps_per_epoch), total=steps_per_epoch):
-            pixellink.train()
-            optimizer.zero_grad()
-            images = images.to(device)
-            pos_pixel_masks = pos_pixel_masks.to(device)
-            neg_pixel_masks = neg_pixel_masks.to(device)
-            pixel_weights = pixel_weights.to(device)
-            link_masks = link_masks.to(device)
-            pixel_input, link_input = pixellink(images)
-            # loss_object = net.PixelLinkLoss(pixel_input, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_input, link_masks)
-            loss_object = net.PixelLinkFocalLoss(pixel_input, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_input, link_masks)
-            loss_object.loss.backward()
-            optimizer.step()
-            steps += 1
-            reporter.step(loss_object, steps)
-        reporter.flush(steps)
-
-        pixellink.eval()
-        test_steps_per_epoch = (len(test_dataset) - 1) // 8 + 1
-        with torch.no_grad():
-            for images, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_masks in tqdm(test_dataloader, total=test_steps_per_epoch):
+    def step_fn(training):
+        def fn(engine, batch):
+            if training:
+                pixellink.train()
+            else:
+                pixellink.eval()
+            with torch.set_grad_enabled(training):
+                images, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_masks = batch
+                if training:
+                    optimizer.zero_grad()
                 images = images.to(device)
                 pos_pixel_masks = pos_pixel_masks.to(device)
                 neg_pixel_masks = neg_pixel_masks.to(device)
@@ -139,24 +80,105 @@ def main():
                 pixel_input, link_input = pixellink(images)
                 # loss_object = net.PixelLinkLoss(pixel_input, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_input, link_masks)
                 loss_object = net.PixelLinkFocalLoss(pixel_input, pos_pixel_masks, neg_pixel_masks, pixel_weights, link_input, link_masks)
-                test_reporter.step(loss_object, steps)
+                if training:
+                    loss_object.loss.backward()
+                    optimizer.step()
+                return {
+                    "loss": loss_object.loss.item(),
+                    "loss/pixel": loss_object.pixel_loss.item(),
+                    "loss/link": loss_object.link_loss.item(),
+                    "accuracy/pixel": loss_object.pixel_accuracy,
+                    "accuracy/link": np.mean(loss_object.link_accuracy),
+                }
+        return fn
 
-        current_score = np.mean(test_reporter.pixel_accuracies) * np.mean(test_reporter.link_accuracies)
-        # scheduler.step(current_loss)
-        best_score = current_score
-        state_dict = {
-            "pixellink": pixellink.state_dict(),
-            "optimizer": optimizer.state_dict(),
-            "steps": steps,
-            "epoch": epoch,
-            "scale": args.scale,
-            "best_score": best_score,
-        }
-        checkpoint_path = os.path.join(args.checkpoint, "epoch-{}.pth.tar".format(epoch))
-        print("[Epoch {} Step {}] Save checkpoint {}".format(epoch, steps, checkpoint_path))
-        torch.save(state_dict, checkpoint_path)
-        test_reporter.flush(steps)
+    writer = SummaryWriter(os.path.join(args.logdir, "train"))
+    test_writer = SummaryWriter(os.path.join(args.logdir, "test"))
 
+    trainer = Engine(step_fn(training=True))
+    evaluator = Engine(step_fn(training=False))
+
+    checkpoint_handler = ModelCheckpoint(
+        args.checkpoint,
+        "networks",
+        n_saved=5,
+        require_empty=False,
+        score_function=lambda engine: -engine.state.metrics["loss"],
+        score_name="loss")
+    evaluator.add_event_handler(Events.COMPLETED, handler=checkpoint_handler,
+                                to_save={"net": pixellink})
+    timer = Timer(average=True)
+
+    monitoring_metrics = ["loss", "loss/pixel", "loss/link", "accuracy/pixel", "accuracy/link"]
+    for metric in monitoring_metrics:
+        def output_transform(m):
+            def fn(x):
+                return x[m]
+            return fn
+        RunningAverage(output_transform=output_transform(metric)).attach(trainer, metric)
+        RunningAverage(output_transform=output_transform(metric)).attach(evaluator, metric)
+
+    pbar = ProgressBar()
+    pbar.attach(trainer, metric_names=monitoring_metrics)
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def print_logs(engine):
+        if (engine.state.iteration - 1) % LOG_FREQ != 0:
+            return
+        for key, value in engine.state.metrics.items():
+            writer.add_scalar(key, value, engine.state.iteration)
+
+        message = "[{epoch}/{max_epoch}][{i}/{max_i}] (train)\t".format(
+            epoch=engine.state.epoch,
+            max_epoch=args.epochs,
+            i=(engine.state.iteration % len(dataloader)),
+            max_i=len(dataloader),
+        )
+        for key, value in engine.state.metrics.items():
+            message += ' | {key}: {value}'.format(key=key, value=str(round(value, 5)))
+        pbar.log_message(message)
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def print_validation_results(engine):
+        if (engine.state.iteration - 1) % LOG_FREQ != 0:
+            return
+        evaluator.run(test_dataloader)
+        for key, value in evaluator.state.metrics.items():
+            test_writer.add_scalar(key, value, engine.state.iteration)
+
+        message = "[{epoch}/{max_epoch}][{i}/{max_i}] (test) \t".format(
+            epoch=engine.state.epoch,
+            max_epoch=args.epochs,
+            i=(engine.state.iteration % len(dataloader)),
+            max_i=len(dataloader),
+        )
+        for key, value in evaluator.state.metrics.items():
+            message += ' | {key}: {value}'.format(key=key, value=str(round(value, 5)))
+        pbar.log_message(message)
+
+    timer.attach(trainer, start=Events.EPOCH_STARTED,
+                 resume=Events.ITERATION_STARTED,
+                 pause=Events.ITERATION_COMPLETED,
+                 step=Events.ITERATION_COMPLETED)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def print_time(engine):
+        pbar.log_message("Epoch {} done. Time per batch: {:.3f}[s]".format(
+            engine.state.epoch,
+            timer.value(),
+        ))
+        timer.reset()
+
+    @trainer.on(Events.EXCEPTION_RAISED)
+    def handle_exception(engine, e):
+        if isinstance(e, KeyboardInterrupt) and (engine.state.iteration > 1):
+            engine.terminate()
+            warnings.warn("KeyboardInterrupt caught. Exiting gracefully.")
+            checkpoint_handler(engine, {"net": pixellink})
+        else:
+            raise e
+
+    trainer.run(dataloader, args.epochs)
     writer.close()
     test_writer.close()
 
